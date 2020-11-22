146b456d5231eaaa9aade4cfb1cf28c747a6b37a
dogless <2295841729@qq.com>
2020-11-22 18:44:53 +0800

    DoubleClickCloseWindow.user.js

---
+++ DoubleClickCloseWindow.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         DoubleClickCloseWindow
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match      *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.body.ondblclick = function(){
+       window.close()
+    }
+})();
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # monkeyScript
-自制油猴脚本
+自制自用 油猴脚本
 
 |  脚本  | 功能  |
 |  ----  | ----  |
 | TXTReader.user.js  | 以Chrome作为文本阅读器，自定义背景图片，文字大小等 |
-
+|DoubleClickCloseWindow.user.js| 双击关闭标签页|
