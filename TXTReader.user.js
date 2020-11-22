15499e2f009794d7a5bc4433465440e192e197d1
dogless <2295841729@qq.com>
2020-11-22 18:24:05 +0800

    TXTReader.user.js

---
+++ TXTReader.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         TXTReader
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include        file:///*/*.txt
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.body.style.background='url(C:/Users/x/Pictures/txt/1.jpg)';
+    document.body.style.backgroundSize='100%';
+    document.body.style.backgroundColor='rgba(232, 204, 215, 0.9)';
+    document.body.style.backgroundBlendMode='overlay';
+    document.body.style.fontSize=24;
+})();
