26f9edaaae5346cf5a84388284c7c8822ec8e9c4
dogless <2295841729@qq.com>
2020-12-01 21:25:52 +0800

    fix include

--- ZhihuPass.js
+++ ZhihuPass.js
@@ -1,17 +1,17 @@
 // ==UserScript==
-// @name        ZhiHuPass
-// @namespace   Violentmonkey Scripts
+// @name        知乎免登陆12月1号
 // @match       *://*zhihu*/*
+// @include       *://*zhihu*/*
 // @grant       none
-// @version     1.0
+// @version     2.0
 // @require     https://cdn.bootcdn.net/ajax/libs/jquery/3.3.1/jquery.js
-// @author      -
+// @author      https://github.com/doglex
 // @run-at document-end
 // @description 2020/11/30 下午11:37:13
+// @namespace https://greasyfork.org/users/710655
 // ==/UserScript==
-
+console.log("免登陆功能已安装")
 setInterval( () =>{
-        console.log("免登陆功能已安装")
         $(".Modal-wrapper").remove()
         $(".Modal-enter-done").remove()
         document.documentElement.style.overflow='auto'
